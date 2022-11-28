bf9a3d80b2dd151776cdc460fe995dd39a3e0bc3
awwalm (work) <67423428+awwalm@users.noreply.github.com>
2022-11-28 17:00:09 +0400

    Uodated implementation of search with Embeddings

--- OpenAIAPI/search.py
+++ OpenAIAPI/search.py
@@ -1,18 +1,36 @@
 import os
-import openai
+import requests
+import json
+import numpy as np
 
-# JSONL content documents
-doc_json = '{"text": "plane"}\n{"text": "boat"}\n{"text": "spaceship"}\n{"text":"car"}'
-with open("doc.jsonl", "w") as doc:
-    doc.write(doc_json)
-doc.close()
+# API key
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
-# Sending the query and documents to the API
-openai.api_key = os.environ["OPENAI_API_KEY"]
-response = openai.File.create(file=open("doc.jsonl"), purpose="search")
-search_response = openai.Engine("babbage").search(
-    search_model="babbage",
-    query="A vehicle with wheels"
-)
+# Search endpoint (https://api.openai.com/v1/engines/<engine_id>/search) is deprecated,
+# Embeddings is the updated means of conducting semantic search
+URL = "https://api.openai.com/v1/embeddings"
 
-print(search_response)
+# Headers
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + OPENAI_API_KEY
+}
+
+# Data to be sent to the server
+query = "A vehicle with wheels"
+documents = ("plane", "boat", "spaceship", "car", query)
+data = json.dumps({
+    "model": "text-search-babbage-query-001",
+    "input": documents
+})
+
+# Process response and store embeddings in dictionary
+result = requests.post(URL, headers=headers, data=data)
+embeddings = {documents[n]: result.json()["data"][n]["embedding"] for n in range(len(documents))}
+
+# Evaluate embedding scores using dot products
+dot_products = [
+    np.dot(embeddings[query], embeddings[documents[n]]) for n in range(len(documents)-1)
+]
+
+print(f"{query} is most similar to {documents[dot_products.index(max(dot_products))]}")
